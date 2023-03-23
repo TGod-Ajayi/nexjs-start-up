be6bfffc33e5516f45ce4c3073e3e94597d676fc
ThankGod Ajayi <thankiziedon@gmail.com>
2023-03-23 11:23:11 +0530

    added ESLint

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -2,13 +2,22 @@ import { FC } from 'react';
 import { ICardProps } from './card.types';
 
 const Card: FC<ICardProps> = ({ cardName, currNumber }) => {
-	return (
-		<>
-			<div style={{ height: '40px', width: '40px', backgroundColor: 'red' }}>
-				{cardName}
-				{currNumber}
-			</div>
-		</>
-	);
+  const someObj = {
+    a: 1,
+    b: 3,
+    c: 4,
+  };
+  interface b {
+    some: 'sd';
+  }
+
+  return (
+    <>
+      <div style={{ height: '40px', width: '40px', backgroundColor: 'red' }}>
+        {cardName}
+        {currNumber}
+      </div>
+    </>
+  );
 };
 export default Card;
