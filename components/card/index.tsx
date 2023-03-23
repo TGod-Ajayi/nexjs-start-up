d67b0c1dc41f0ee59d8d512d05af09f694530b85
ThankGod Ajayi <thankiziedon@gmail.com>
2023-03-23 11:50:37 +0530

    removed errors

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -1,16 +1,8 @@
 import { FC } from 'react';
+
 import { ICardProps } from './card.types';
 
 const Card: FC<ICardProps> = ({ cardName, currNumber }) => {
-  const someObj = {
-    a: 1,
-    b: 3,
-    c: 4,
-  };
-  interface b {
-    some: 'sd';
-  }
-
   return (
     <>
       <div style={{ height: '40px', width: '40px', backgroundColor: 'red' }}>
